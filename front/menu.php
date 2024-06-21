8eb0430d6e9dae1df142f608b4db7e19519c7b0e
mercadoCODING <raphaelandremercado@gmail.com>
2024-06-21 17:42:56 +0800

    base

---
+++ front/menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Group 7">
+    <title>Menu</title>
+</head>
+
+<body>
+    
+</body>
+</html>
